8e49eab843d02002ef527193c799d7cc04a047f1
toyota-m2k <toyota.mitsuki@gmail.com>
2025-07-31 20:25:10 +0900

    libs

--- libWorker/build.gradle.kts
+++ libWorker/build.gradle.kts
@@ -42,8 +42,8 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.work.runtime)
-    implementation(libs.androidx.work.runtime.ktx)
+    api(libs.androidx.work.runtime)
+    api(libs.androidx.work.runtime.ktx)
     implementation(libs.material)
 
 //    implementation(libs.android.utilities)
